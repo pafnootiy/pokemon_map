542598d6bbad3779f971440e47eb68ea42226046
pafnootiy <ksander-86@mail.ru>
2022-11-01 18:37:47 +0300

    remove previous_evolution from try block

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -68,14 +68,14 @@ def show_pokemon(request, pokemon_id):
         'description': pokemon.description,
         'title_en': pokemon.title_en,
         'title_jp': pokemon.title_jp}
+    if pokemon.previous_evolution:
+        serialized_pokemon['previous_evolution'] = {
+            'pokemon_id': pokemon.previous_evolution.id,
+            'img_url': pokemon.previous_evolution.picture.url,
+            'title_ru': pokemon.previous_evolution.title
+        }
     try:
         next_evolution = pokemon.next_evolutions.get()
-        if pokemon.previous_evolution:
-            serialized_pokemon['previous_evolution'] = {
-                'pokemon_id': pokemon.previous_evolution.id,
-                'img_url': pokemon.previous_evolution.picture.url,
-                'title_ru': pokemon.previous_evolution.title
-            }
         if next_evolution:
             serialized_pokemon['next_evolution'] = {
                 'pokemon_id': next_evolution.id,
